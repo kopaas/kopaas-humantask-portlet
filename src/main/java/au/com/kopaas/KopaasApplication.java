6992b528de5d6f277dfff499cd30fab236005eca
prashan <prashan@kopaas.com.au>
2013-10-21 23:46:19 +1100

    Initial human task portlet implementation in Vaadin (WIP). Includes maven build to generate the portlet war file. Includes Vaadin libraries.

---
+++ src/main/java/au/com/kopaas/KopaasApplication.java
@@ -0,0 +1,33 @@
+package au.com.kopaas;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+@Theme("reindeer")
+public class KopaasApplication extends UI {
+	
+	@VaadinServletConfiguration(productionMode = false, ui = KopaasApplication.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Label label = new Label("Welcome to Human Task Portlet");
+		layout.addComponent(label);
+
+	}
+
+}
